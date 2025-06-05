229ff8fae00a8dbf567265d82b3f069e360d8026
igyyyy <piauigor602@gmail.com>
2025-06-05 15:09:38 -0300

    Remoção do contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,9 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Igor',
     })
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
