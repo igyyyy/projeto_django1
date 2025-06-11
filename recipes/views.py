8880bf918c9e2ffcf307fc09c51dc002d9b8250f
igyyyy <piauigor602@gmail.com>
2025-06-11 19:23:49 -0300

    Criação da view "recipe" para cada receita.

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Igor',
     })
+
+# Create your views here.
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Igor',
+    })
